d0b2e9f9f8a213e2aa5663434995a48c795344ef
abdoforda <43405421+abdoforda@users.noreply.github.com>
2022-02-15 17:53:49 +0200

    d

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,89 @@
-@extends('layouts.app')
+<html>
+<head>
+<meta charset="utf-8" lang= "fr" />
+<link href="{{ asset('css/style1.css') }}" id="theme1" rel="stylesheet">
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<script src="https://use.fontawesome.com/948cc87750.js"></script>
+<script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>OprNize</title>
+</head>
+<header class="header">
 
-<div class="account-pages my-5 pt-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="text-center mb-5">
-                    <a href="index.html" class="logo"><img src="assets/images/logo-dark.png" height="24"
-                            alt="logo"></a>
-                            <br /><br />
-                    <h5 class="font-size-16 mb-4">{{__('Oprnize Technical Services Company')}}</h5>
+	<div class="entrer">
+		<div class="lang">ENGLISH</div>
+		<a href="/workspace"">
+            <div class="oprnize">تسجيل الدخول</div>
+        </a>
+        
+	</div>
 
-                    <h4 class="mt-5">{{__('You can start now')}}</h4>
+	<nav>
+		<ul>
+			<li><a href="#">أسعارنا</a></li>
+			<li><a href="#"> تعرف علينا</a></li>
+			<li><a href="#"> خدماتنا</a></li>
+			<li><h1 class="oprnize">OprNize</h1></li>
+		</ul>
+	</nav>
+</header>
 
-                    <a href="/workspace"><button type="button" class="btn btn-outline-info waves-effect waves-light">{{__('Log in here')}}</button></a>
-                    <a href="/register"><button type="button" class="btn btn-outline-info waves-effect waves-light">{{__('Free 30 day trial')}}</button></a>
-
-                    <div class="mt-4">
-                        <img src="assets/images/coming-soon.png" class="img-fluid" alt="">
-                    </div>
-
-                    <div class="row justify-content-center mt-5 pt-3">
-                        <div class="col-md-8">
-                            <div data-countdown="2021/12/31" class="counter-number"></div>
-                        </div> <!-- end col-->
-                    </div> <!-- end row-->
-                </div>
-            </div>
-        </div>
-        <!-- end row -->
-    </div>
-</div>
+<body>
+	<div class="center">
+		<div><img src="img/im1.png"/></div>
+		<section>
+			<div class="oprnize">اوبرنايز</div>		
+			<div class="securite">نحو تحول رقمي امن</div>
+			<div class="p">
+				<div>لتحمل عبء عمليات الموارد البشرية اليومية</div>
+				<div id="buts">لتركز على تحقيق اهدافك العظمى</div>
+			</div>
+			<a href="/register"><div id="start">ابدأ التجربة الان</div></a>
+			<div class="true">
+				<div>
+					<img id="true1"class="trueimg" src="img/tru2.png"style="width:1rem;"/>
+					<span>45 يوما مجانا</span>
+				</div>
+				<div>
+					<img class="trueimg" src="img/tru2.png"style="width:1rem;"/>
+					<span>لا معلومات مالية مطلوبة</span>
+				</div>
+				<div>
+					<img class="trueimg" src="img/tru2.png"style="width:1rem;"/>
+					<span>يمكنك الالغاء في اي وقت</span>
+				</div>
+			</div>
+			
+		</section>
+	</div>
+	
+	<footer>
+		<div class="div1" >
+        <i class="fa fa-database" id="img1" style="font-size:55px;"></i>
+			<div>
+				حاسبة </br>المخالفات الخدمة 
+			</div>
+		</div>
+		<div class="div1">
+			<div style='position:relative;padding:0.8rem;'>
+				<span class="iconify" data-icon="bx:bxs-dollar-circle" 
+				data-flip="horizontal,vertical"
+				style='font-size:60px;'></span>		
+				<div>
+					<img  id="img2" src="img/interdit1.png"  style="width:1.5rem;"/>
+				</div>
+			</div>
+			<div>
+				حاسبة </br> المخالفات العمالية
+			</div>
+		</div>
+		<div class="div1" onmouseout="normalImg()">
+			<i class='fas fa-stopwatch' id="img3" style='font-size:55px;' ></i>
+			<div>
+				حاسبة </br> العمل الاضافي
+			</div>
+		</div>
+	</footer>
+</body>
+<html>
